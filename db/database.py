17c013ed4f7693f37760c2823187c83e1065fce1
Evgeni Stroikin <evgenistroikin@gmail.com>
2024-06-24 19:25:08 +0700

    Модифицировал подключение к локальной базе в database.py, добавлено .рус в гитигнор

--- db/database.py
+++ db/database.py
@@ -1,21 +1,21 @@
 # Файл database.py
-import os
-from sqlalchemy import create_engine
+from ugkorea.db.config import local_db_config
+import sqlalchemy
+from sqlalchemy import create_engine, text
 
 
 def get_db_engine():
-    db_user = os.getenv('DB_USER', 'postgres')
-    db_password = os.getenv('DB_PASSWORD', 'default_password')
-    db_host = os.getenv('DB_HOST', 'localhost')
-    db_port = os.getenv('DB_PORT', '5432')
-    db_name = os.getenv('DB_NAME', 'ugkorea')
-    db_url = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
-    
+    connection_string = f"postgresql+psycopg2://{local_db_config['user']}:{local_db_config['password']}@{local_db_config['host']}:{local_db_config['port']}/{local_db_config['database']}"
+    engine = create_engine(connection_string)
+
     try:
-        engine = create_engine(db_url)
-        # Если подключение успешно, выведите сообщение об успешном подключении
-        print("Успешное подключение к базе данных.")
-        return engine
-    except Exception as e:
-        print(f"Ошибка при подключении к базе данных: {e}")
-        return None
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+            if result.scalar() == 1:
+                print("Connection to the database has been established successfully.")
+            else:
+                print("Connection test failed.")
+    except sqlalchemy.exc.SQLAlchemyError as e:
+        print(f"An error occurred: {e}")
+
+    return engine
