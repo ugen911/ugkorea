9d08c236937d77c9adcf8bc1ad94823e0bb9a330
Евгений Стройкин <evgenistroikin@gmail.com>
2024-08-01 14:33:43 +0700

    Убрал упущенный спрос из регламентной операции

--- Update_all_date.bat
+++ Update_all_date.bat
@@ -5,8 +5,6 @@ python -m reglament_task.update_analitics_gl
 echo Updated all date from analitic 1c.
 python -m reglament_task.update_price_stock_old
 echo From old prices update.
-python -m reglament_task.upushspros
-echo Upushennii spros ready.
 python -m reglament_task.nomenk_class
 echo Nomenklatura classification ready.
 python -m accessold.statisticforacc
