02516b91d8fb1b7fc40682f1ef369d06936bfa99
Евгений Стройкин <evgenistroikin@gmail.com>
2024-09-03 17:07:18 +0700

    BergAPI.bat +

---
+++ Bergapi.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d %~dp0
+call venv\Scripts\activate
+
+python -m api.bergapi
+echo bergapi completed.
+
+rem Запись информации о выполнении в лог-файл
+echo Процедура %~nx0 выполнена в %date% %time% >> log.txt
+
+timeout /t 5 >nul
+exit
