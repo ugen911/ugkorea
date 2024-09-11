4dc5b1e954ee25bbc1eeefa17ac2bea43590f5f3
Evgeni Stroikin <evgenistroikin@gmail.com>
2024-09-11 09:21:01 +0700

    Исправлен заголовок в отправке файла для drom.ru

--- mail/accesstomail.py
+++ mail/accesstomail.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 from ugkorea.mail.config import email, password
+from email.utils import formataddr
 
 def send_email_via_mailru(to_email, subject, body, attachment_path=None, attachment_name=None):
     from_email = email  # Ваш Mail.ru email
@@ -11,7 +12,7 @@ def send_email_via_mailru(to_email, subject, body, attachment_path=None, attachm
 
     # Создаем объект MIMEMultipart
     msg = MIMEMultipart()
-    msg['From'] = from_email
+    msg['From'] = formataddr(('Your Name', from_email))  # Можно указать имя отправителя
     msg['To'] = to_email
     msg['Subject'] = subject
     msg['Cc'] = from_email  # Добавляем копию самому себе
@@ -27,11 +28,11 @@ def send_email_via_mailru(to_email, subject, body, attachment_path=None, attachm
             encoders.encode_base64(part)
             if not attachment_name:
                 attachment_name = attachment_path.split('/')[-1]
-            
+
             # Добавляем правильные заголовки для имени файла и типа контента
             part.add_header(
                 'Content-Disposition',
-                f'attachment; filename="{attachment_name}"'
+                f'attachment; filename*="UTF-8\'\'{attachment_name}"'
             )
             part.add_header(
                 'Content-Type',
@@ -41,10 +42,16 @@ def send_email_via_mailru(to_email, subject, body, attachment_path=None, attachm
             msg.attach(part)
 
     # Устанавливаем соединение с сервером и отправляем письмо
-    server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    server.login(from_email, from_password)
-    text = msg.as_string()
-    server.sendmail(from_email, [to_email, from_email], text)  # Отправляем письмо и получателю, и себе
-    server.quit()
+    try:
+        server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+        server.login(from_email, from_password)
+        text = msg.as_string()
+        server.sendmail(from_email, [to_email, from_email], text)  # Отправляем письмо и получателю, и себе
+        print(f"Email sent to {to_email} with attachment {attachment_name}, and cc to {from_email}")
+    except Exception as e:
+        print(f"Failed to send email: {str(e)}")
+    finally:
+        server.quit()
+
 
     print(f"Email sent to {to_email} with attachment {attachment_name}, and cc to {from_email}")
