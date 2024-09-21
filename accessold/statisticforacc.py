38b5b5481f97e60ea9a8d8d33dba053cd1cb39c0
Евгений Стройкин <evgenistroikin@gmail.com>
2024-09-21 09:25:58 +0700

    Добавлен подсчет не только продаж но и поступлений в количестве в statisticforacc.py

--- accessold/statisticforacc.py
+++ accessold/statisticforacc.py
@@ -38,8 +38,50 @@ with engine.connect() as conn:
     count = result.fetchone()[0]
 
 if count > 0:
-    print(f"Таблица успешно сохранена в базу данных. Количество записей: {count}")
+    print(f"Таблица salespivot успешно сохранена в базу данных. Количество записей: {count}")
 else:
-    print("Ошибка при сохранении таблицы в базу данных.")
+    print("Ошибка при сохранении таблицы salespivot в базу данных.")
 
+# Вывод первых 20 строк из таблицы salespivot
+print("Первые 20 строк из таблицы salespivot:")
+sales_pivot_first_20 = pd.read_sql("SELECT * FROM public.salespivot LIMIT 20", engine)
+print(sales_pivot_first_20)
 
+# Добавляем работу с таблицей postuplenija
+query_postuplenija = "SELECT kod, kolichestvo, data FROM postuplenija"
+postuplenija_df = pd.read_sql(query_postuplenija, engine)
+postuplenija_df.name = "postuplenija"
+
+# Преобразуем колонку data в формат datetime для postuplenija_df
+postuplenija_df['data'] = pd.to_datetime(postuplenija_df['data'], format='%Y-%m-%d %H:%M:%S')
+
+# Добавляем колонку year_month для группировки по месяцам
+postuplenija_df['year_month'] = postuplenija_df['data'].dt.to_period('M')
+
+# Группируем данные по kod и year_month и суммируем kolichestvo
+postuplenija_df['kolichestvo'] = pd.to_numeric(postuplenija_df['kolichestvo'], errors='coerce').fillna(0)
+monthly_supplies = postuplenija_df.groupby(['kod', 'year_month'])['kolichestvo'].sum().reset_index()
+
+# Преобразуем данные в длинный формат
+supplies_long = monthly_supplies.pivot_table(index=['kod', 'year_month'], values='kolichestvo').reset_index()
+
+# Преобразуем столбцы year_month в строки
+supplies_long['year_month'] = supplies_long['year_month'].astype(str)
+
+# Сохраняем результат в таблицу базы данных
+supplies_long.to_sql('suppliespivot', engine, schema='public', if_exists='replace', index=False)
+
+# Проверка успешности сохранения
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT COUNT(*) FROM public.suppliespivot"))
+    count = result.fetchone()[0]
+
+if count > 0:
+    print(f"Таблица suppliespivot успешно сохранена в базу данных. Количество записей: {count}")
+else:
+    print("Ошибка при сохранении таблицы suppliespivot в базу данных.")
+
+# Вывод первых 20 строк из таблицы suppliespivot
+print("Первые 20 строк из таблицы suppliespivot:")
+supplies_pivot_first_20 = pd.read_sql("SELECT * FROM public.suppliespivot LIMIT 20", engine)
+print(supplies_pivot_first_20)
