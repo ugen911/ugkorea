72d528b7fa3ad96e6ee083074622b32a71ffdd84
Евгений Стройкин <evgenistroikin@gmail.com>
2024-09-29 19:34:38 +0700

    Добавлена корректировка на аналоги по дате создания позиции

--- statistic/calculation.py
+++ statistic/calculation.py
@@ -107,9 +107,8 @@ def calculate_sales_metrics(sales_data: pd.DataFrame, union_data: pd.DataFrame) 
 
     # Calculate 'min_stock' based on the given logic
     def calculate_min_stock(row):
-    # Проверяем, являются ли средние продажи или стандартное отклонение NaN
         if pd.isna(row['mean_sales_last_12_months']) or pd.isna(row['std_sales_last_12_months']):
-            return 0  # Возвращаем 0, если нет данных для расчетов
+            return 0  # Return 0 if no data for calculations
 
         if row['ABC'] in ['A', 'A1']:
             return round((row['mean_sales_last_12_months'] * 1 + row['std_sales_last_12_months']) + 0.49)
@@ -119,7 +118,6 @@ def calculate_sales_metrics(sales_data: pd.DataFrame, union_data: pd.DataFrame) 
             return 1
         else:
             return round((row['mean_sales_last_12_months'] + row['std_sales_last_12_months']) + 0.49)
-
 
     union_data['min_stock'] = union_data.apply(calculate_min_stock, axis=1)
 
@@ -141,6 +139,20 @@ def calculate_sales_metrics(sales_data: pd.DataFrame, union_data: pd.DataFrame) 
         return row['min_stock']
 
     union_data['min_stock'] = union_data.apply(adjust_min_stock, axis=1)
+
+    # Further adjust 'min_stock' if 'datasozdanija' is in the current or previous month
+    def adjust_min_stock_for_creation_date(row):
+        datasozdanija_date = pd.to_datetime(row['datasozdanija'], format='%d.%m.%Y %H:%M:%S', errors='coerce')
+        if datasozdanija_date is not pd.NaT:
+            creation_period = pd.Period(datasozdanija_date.strftime('%Y-%m'), freq='M')
+            if creation_period in [current_period, current_period - 1]:
+                # Find the maximum 'min_stock' for the same 'gruppa_analogov'
+                max_min_stock = union_data[union_data['gruppa_analogov'] == row['gruppa_analogov']]['min_stock'].max()
+                if max_min_stock > row['min_stock']:
+                    return max_min_stock
+        return row['min_stock']
+
+    union_data['min_stock'] = union_data.apply(adjust_min_stock_for_creation_date, axis=1)
     
     return union_data
 
