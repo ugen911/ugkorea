8e7b0b6826c3a23bd3efe723bc3196c32434da69
Евгений Стройкин <evgenistroikin@gmail.com>
2024-10-16 11:52:15 +0700

    Промежуточный неликвид

--- reprising/nonliquid.py
+++ reprising/nonliquid.py
@@ -13,12 +13,19 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
         filtered_df["data"], format="%d.%m.%Y %H:%M", errors="coerce"
     )
 
+    # Фильтр для наличия 'api' в колонке delsklad
+    api_filter = filtered_df["delsklad"].str.contains("api", case=False, na=False)
+
     # Фильтруем позиции с рассчитанным new_price, если delprice или median_price заполнены и delsklad содержит "api"
     condition = (
         filtered_df["new_price"].notna()
         & (filtered_df["delprice"].notna() | filtered_df["median_price"].notna())
-        & filtered_df["delsklad"].str.contains("api", case=False, na=False)
+        & api_filter
     )
+
+    # Добавляем колонки для процента наценки и изменения в рублях
+    filtered_df["base_percent"] = np.nan
+    filtered_df["price_diff"] = np.nan
 
     # Проверка на наличие покупок за указанный период
     def is_non_liquid(data):
@@ -46,13 +53,9 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
         sales_threshold=False,
         sales_period=None,
     ):
-        # Используем правильные колонки: median_price для средней цены и delprice для закупочной цены
-        median_price = row[
-            "median_price"
-        ]  # Колонка median_price - средняя цена на проценках
-        delprice = row["delprice"]  # Закупочная цена товара
+        median_price = row["median_price"]
+        delprice = row["delprice"]
 
-        # Если обе цены (median_price и delprice) заполнены
         if pd.notna(median_price) and pd.notna(delprice):
             lower_bound = max(
                 median_price * lower_bound_percent, delprice * lower_bound_percent
@@ -60,13 +63,9 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
             upper_bound = max(
                 median_price * upper_bound_percent, delprice * upper_bound_percent
             )
-
-        # Если есть только median_price
         elif pd.notna(median_price):
             lower_bound = median_price * lower_bound_percent
             upper_bound = median_price * upper_bound_percent
-
-        # Если есть только delprice
         elif pd.notna(delprice):
             lower_bound = delprice * lower_bound_percent
             upper_bound = delprice * upper_bound_percent
@@ -74,20 +73,19 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
         # Наценка при наличии более двух месяцев с продажами
         if sales_threshold:
             if sales_period == "24+":
-                upper_bound *= 1.2  # Для товаров с отсутствием покупок более 24 месяцев
+                upper_bound *= 1.2
             elif sales_period == "18-24":
-                upper_bound *= 1.25  # Для товаров с отсутствием покупок 18-24 месяцев
+                upper_bound *= 1.25
             elif sales_period == "12-18":
-                upper_bound *= 1.37  # Для товаров с отсутствием покупок 12-18 месяцев
+                upper_bound *= 1.37
 
-        # Округляем новую цену до ближайших 10
         new_price = np.ceil(upper_bound / 10) * 10
         return new_price
 
     # Применяем фильтры
     for index, row in filtered_df[condition].iterrows():
         kod = row["kod"]
-        last_purchase_date = row["data"]  # Дата последнего прихода
+        last_purchase_date = row["data"]
 
         # Логика для полностью неликвидных позиций (не покупались 24 месяца и больше)
         if is_non_liquid(last_purchase_date):
@@ -97,19 +95,6 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
                 )
             else:
                 filtered_df.at[index, "new_price"] = calculate_price(row, 0.7, 0.8)
-
-            # Проверка на maxprice + 30% и median_price + 50%
-            maxprice = row["maxprice"]
-            medianprice = row["median_price"]
-
-            if pd.notna(maxprice) and pd.notna(medianprice):
-                max_limit = np.ceil((maxprice * 1.3) / 10) * 10
-                median_limit = np.ceil((medianprice * 1.5) / 10) * 10
-                final_limit = max(max_limit, median_limit)
-
-                # Если new_price ниже пределов maxprice или median_price, подтягиваем его
-                if row["new_price"] < final_limit:
-                    filtered_df.at[index, "new_price"] = final_limit
 
         # Логика для неликвида (не покупались 18-24 месяца)
         elif (
@@ -122,19 +107,6 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
             else:
                 filtered_df.at[index, "new_price"] = calculate_price(row, 0.8, 0.9)
 
-            # Проверка на maxprice + 30% и median_price + 50%
-            maxprice = row["maxprice"]
-            medianprice = row["median_price"]
-
-            if pd.notna(maxprice) and pd.notna(medianprice):
-                max_limit = np.ceil((maxprice * 1.3) / 10) * 10
-                median_limit = np.ceil((medianprice * 1.5) / 10) * 10
-                final_limit = max(max_limit, median_limit)
-
-                # Если new_price ниже пределов maxprice или median_price, подтягиваем его
-                if row["new_price"] < final_limit:
-                    filtered_df.at[index, "new_price"] = final_limit
-
         # Логика для неликвида (не покупались 12-18 месяцев)
         elif (
             last_purchase_date <= last_18_months and last_purchase_date > last_12_months
@@ -146,17 +118,48 @@ def adjust_new_price_for_non_liquid(filtered_df, salespivot):
             else:
                 filtered_df.at[index, "new_price"] = calculate_price(row, 1.0, 1.1)
 
-            # Проверка на maxprice + 20% и median_price + 40%
-            maxprice = row["maxprice"]
-            medianprice = row["median_price"]
+        # Проверка и корректировка на maxprice и median_price
+        maxprice = row["maxprice"]
+        medianprice = row["median_price"]
 
-            if pd.notna(maxprice) and pd.notna(medianprice):
+        if pd.notna(maxprice) and pd.notna(medianprice):
+            if is_non_liquid(last_purchase_date):
+                max_limit = np.ceil((maxprice * 1.3) / 10) * 10
+                median_limit = np.ceil((medianprice * 1.5) / 10) * 10
+            elif (
+                last_purchase_date <= last_24_months
+                and last_purchase_date > last_18_months
+            ):
+                max_limit = np.ceil((maxprice * 1.3) / 10) * 10
+                median_limit = np.ceil((medianprice * 1.5) / 10) * 10
+            elif (
+                last_purchase_date <= last_18_months
+                and last_purchase_date > last_12_months
+            ):
                 max_limit = np.ceil((maxprice * 1.2) / 10) * 10
                 median_limit = np.ceil((medianprice * 1.4) / 10) * 10
+            else:
+                max_limit = np.nan
+                median_limit = np.nan
+
+            if pd.notna(max_limit) and pd.notna(median_limit):
                 final_limit = max(max_limit, median_limit)
 
                 # Если new_price ниже пределов maxprice или median_price, подтягиваем его
-                if row["new_price"] < final_limit:
+                if filtered_df.at[index, "new_price"] < final_limit:
                     filtered_df.at[index, "new_price"] = final_limit
+
+    # Вычисление base_percent и разницы в рублях между new_price и tsenarozn для всех строк
+    mask = filtered_df["delprice"].notna() & filtered_df["new_price"].notna()
+    filtered_df.loc[mask, "base_percent"] = (
+        (filtered_df.loc[mask, "new_price"] - filtered_df.loc[mask, "delprice"])
+        / filtered_df.loc[mask, "delprice"]
+    ) * 100
+
+    tsenarozn_mask = filtered_df["tsenarozn"].notna() & filtered_df["new_price"].notna()
+    filtered_df.loc[tsenarozn_mask, "price_diff"] = (
+        filtered_df.loc[tsenarozn_mask, "new_price"]
+        - filtered_df.loc[tsenarozn_mask, "tsenarozn"]
+    )
 
     return filtered_df
