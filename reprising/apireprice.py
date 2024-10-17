445b58997b1db488c8a12f7515ced8c202ee4e0a
Евгений Стройкин <evgenistroikin@gmail.com>
2024-10-17 11:56:51 +0700

    работает api

--- reprising/apireprice.py
+++ reprising/apireprice.py
@@ -58,6 +58,17 @@ def calculate_new_prices_for_api(
             not sales_data.empty or not supplies_data.empty
         )  # True если были продажи или покупки
 
+    # Функция проверки, были ли продажи за последние 12 месяцев в не более чем 1 месяце
+    def sales_in_last_12_months(kod):
+        last_12_months = current_date - pd.DateOffset(months=12)
+        sales_data = salespivot[
+            (salespivot["kod"] == kod) & (salespivot["year_month"] >= last_12_months)
+        ]
+        # Проверяем, если данные пусты или последняя дата продажи старше 12 месяцев
+        if sales_data.empty or sales_data["year_month"].max() < last_12_months:
+            return True  # Нет продаж за последние 12 месяцев или последняя продажа была больше 12 месяцев назад
+        return len(sales_data) <= 1  # True если продажи были в не более чем 1 месяце
+
     # Применяем логику уменьшения наценки
     def get_base_percent(row):
         kod = row["kod"]
@@ -147,27 +158,29 @@ def calculate_new_prices_for_api(
         .mean()
     )
 
-    # Сравниваем с товарами, где sklad содержит "api"
+    # Сравниваем с товарами, где sklad содержит "api" и которые не продавались в последние 12 месяцев или продавались в 1 месяц
     for index, row in filtered_df.loc[condition_api].iterrows():
-        group_key = (row["type_detail"], row["proizvoditel"])
-        if group_key in group_means:
-            group_mean_price = group_means[group_key]
-            if row["new_price"] > group_mean_price * 1.2:
-                # Снижаем new_price до средней цены + 10%
-                filtered_df.at[index, "new_price"] = (
-                    np.ceil(group_mean_price * 1.1 / 10) * 10
-                )
+        kod = row["kod"]
+        if sales_in_last_12_months(kod):
+            group_key = (row["type_detail"], row["proizvoditel"])
+            if group_key in group_means:
+                group_mean_price = group_means[group_key]
+                if row["new_price"] > group_mean_price * 1.2:
+                    # Снижаем new_price до средней цены + 10%
+                    filtered_df.at[index, "new_price"] = (
+                        np.ceil(group_mean_price * 1.1 / 10) * 10
+                    )
 
-        # Проверка на минимальное значение +10% к delprice
-        min_delprice = np.ceil(delprice.at[index] * 1.1 / 10) * 10
-        if row["new_price"] < min_delprice:
-            filtered_df.at[index, "new_price"] = min_delprice
+            # Проверка на минимальное значение +10% к delprice
+            min_delprice = np.ceil(delprice.at[index] * 1.1 / 10) * 10
+            if row["new_price"] < min_delprice:
+                filtered_df.at[index, "new_price"] = min_delprice
 
-        # Проверка на минимальное значение +25% к medianprice
-        if not pd.isna(middleprice.at[index]):
-            min_middleprice = np.ceil(middleprice.at[index] * 1.25 / 10) * 10
-            if row["new_price"] < min_middleprice:
-                filtered_df.at[index, "new_price"] = min_middleprice
+            # Проверка на минимальное значение +25% к medianprice
+            if not pd.isna(middleprice.at[index]):
+                min_middleprice = np.ceil(middleprice.at[index] * 1.25 / 10) * 10
+                if row["new_price"] < min_middleprice:
+                    filtered_df.at[index, "new_price"] = min_middleprice
 
     # Проверка: если delprice и new_price отсутствуют, но есть median_price
     missing_price_condition = (
@@ -191,6 +204,21 @@ def calculate_new_prices_for_api(
             new_price = row["tsenarozn"]
 
         filtered_df.at[index, "new_price"] = new_price
+
+
+    # Дополнительная проверка на наценку в зависимости от delprice
+    # Если delprice <= 200, то наценка должна быть не меньше 80%
+    filtered_df.loc[condition_api & (delprice <= 200), "new_price"] = np.maximum(
+        filtered_df.loc[condition_api & (delprice <= 200), "new_price"],
+        np.ceil(delprice * 1.8 / 10) * 10,
+    )
+
+    # Если delprice >= 10000, то наценка должна быть не больше 35%
+    filtered_df.loc[condition_api & (delprice >= 10000), "new_price"] = np.minimum(
+        filtered_df.loc[condition_api & (delprice >= 10000), "new_price"],
+        np.ceil(delprice * 1.35 / 10) * 10,
+    )
+
 
     # Применяем проверки только к строкам, где выполняется условие maxprice и middleprice
     condition_maxprice = (
