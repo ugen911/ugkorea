eba89c94a5e33d0206f4e02e38055aa39089d35a
Евгений Стройкин <evgenistroikin@gmail.com>
2024-10-30 15:09:31 +0700

    в корректировку фоток добавил проверку через каталог выгрузки с сайта

--- reglament_task/fotoadress.py
+++ reglament_task/fotoadress.py
@@ -3,9 +3,10 @@ import re
 import pandas as pd
 from ugkorea.db.database import get_db_engine
 
+
 def process_catalog_files():
     # Получаем путь к папке "Downloads" текущего пользователя
-    folder_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+    folder_path = os.path.join(os.path.expanduser("~"), "Downloads")
 
     try:
         # Проверяем, существует ли папка
@@ -16,14 +17,21 @@ def process_catalog_files():
         files = os.listdir(folder_path)
 
         # Фильтруем файлы, оставляя только нужные
-        catalog_files = [f for f in files if re.match(r'catalog(\s\(\d+\))?\.csv', f)]
+        catalog_files = [f for f in files if re.match(r"catalog(\s\(\d+\))?\.csv", f)]
 
         # Проверяем, есть ли нужные файлы
         if not catalog_files:
             raise FileNotFoundError("Файлы catalog не найдены")
 
         # Сортируем файлы по числу в скобках
-        catalog_files.sort(key=lambda f: int(re.search(r'\((\d+)\)', f).group(1)) if re.search(r'\((\d+)\)', f) else 0, reverse=True)
+        catalog_files.sort(
+            key=lambda f: (
+                int(re.search(r"\((\d+)\)", f).group(1))
+                if re.search(r"\((\d+)\)", f)
+                else 0
+            ),
+            reverse=True,
+        )
 
         # Берем самый свежий файл
         latest_file = catalog_files[0]
@@ -34,36 +42,40 @@ def process_catalog_files():
         # Чтение и обработка файла
         df = pd.read_csv(latest_file_path)
         df = df.dropna()
-        df['photos'] = df['photos'].str.lower().replace({'.jpg':'_big.JPG'}, regex=True)
+        df["photos"] = (
+            df["photos"].str.lower().replace({".jpg": "_big.JPG"}, regex=True)
+        )
 
         def photo(row):
-            plus = 'https://ugkorea.com/pictures/product/big/'
-            string = ''
-            row = row.split(sep=',')
+            plus = "https://ugkorea.com/pictures/product/big/"
+            string = ""
+            row = row.split(sep=",")
             for i in row:
-                string = string + plus + i + ','
+                string = string + plus + i + ","
             return string
 
-        df['adress'] = df['photos'].apply(photo)
-        df.rename(columns={'sku': 'kod'}, inplace=True)
-        
+        df["adress"] = df["photos"].apply(photo)
+        df.rename(columns={"sku": "kod"}, inplace=True)
+
         # Задаем путь для сохранения файла
-        output_dir = os.path.join(os.path.expanduser('~'), 'YandexDisk', 'ЮК')
-        output_path = os.path.join(output_dir, 'photoadress.xlsx')
-        
+        #output_dir = os.path.join(os.path.expanduser("~"), "YandexDisk", "ЮК")
+        #output_path = os.path.join(output_dir, "photoadress.xlsx")
+
         # Проверка доступности директории
-        if os.path.exists(output_dir):
+        #if os.path.exists(output_dir):
             # Сохранение файла
-            df[['kod', 'adress']].to_excel(output_path, index=False)
-            print(f"Файл успешно обработан и сохранен: {output_path}")
-        else:
-            print(f"Директория {output_dir} недоступна. Сохранение файла пропущено.")
+            #df[["kod", "adress"]].to_excel(output_path, index=False)
+            #print(f"Файл успешно обработан и сохранен: {output_path}")
+        #else:
+            #print(f"Директория {output_dir} недоступна. Сохранение файла пропущено.")
 
         # Подключение к базе данных
         engine = get_db_engine()
 
         # Выгрузка данных в таблицу photoadress
-        df[['kod', 'adress']].to_sql('photoadress', con=engine, if_exists='replace', index=False)
+        df[["kod", "adress"]].to_sql(
+            "photoadress", con=engine, if_exists="replace", index=False
+        )
 
         print("Данные успешно выгружены в базу данных в таблицу photoadress.")
 
@@ -72,31 +84,60 @@ def process_catalog_files():
     except Exception as e:
         print(f"Произошла ошибка: {e}")
 
+
 def get_and_process_data():
     # Подключение к базе данных
     engine = get_db_engine()
 
     # Загрузка таблиц из базы данных
-    nomenklaturaold = pd.read_sql('SELECT * FROM nomenklaturaold', con=engine)
-    photoadress = pd.read_sql('SELECT * FROM photoadress', con=engine)
-    stockold = pd.read_sql('SELECT * FROM stockold', con=engine)
+    nomenklaturaold = pd.read_sql("SELECT * FROM nomenklaturaold", con=engine)
+    photoadress = pd.read_sql("SELECT * FROM photoadress", con=engine)
+    stockold = pd.read_sql("SELECT * FROM stockold", con=engine)
 
     # Очистка колонок 'kod' от пробелов
     for df in [nomenklaturaold, photoadress, stockold]:
-        df['kod'] = df['kod'].str.strip()
+        df["kod"] = df["kod"].str.strip()
+
+    # Загрузка файла catalog из папки Downloads
+    folder_path = os.path.join(os.path.expanduser("~"), "Downloads")
+    catalog_files = [
+        f for f in os.listdir(folder_path) if re.match(r"catalog(\s\(\d+\))?\.csv", f)
+    ]
+
+    if catalog_files:
+        # Сортируем файлы по числу в скобках и берем последний
+        catalog_files.sort(
+            key=lambda f: (
+                int(re.search(r"\((\d+)\)", f).group(1))
+                if re.search(r"\((\d+)\)", f)
+                else 0
+            ),
+            reverse=True,
+        )
+        catalog_file_path = os.path.join(folder_path, catalog_files[0])
+
+        # Загружаем и очищаем файл catalog
+        catalog_df = pd.read_csv(catalog_file_path)
+        catalog_df["sku"] = catalog_df["sku"].str.strip()  # Удаление пробелов
+        catalog_df.rename(columns={"sku": "kod"}, inplace=True)
+
+        # Фильтруем основной датафрейм по наличию kod в catalog
+        kod_catalog = catalog_df["kod"].unique()
+    else:
+        print("Файл catalog не найден. Продолжаем без фильтрации по catalog.")
+        kod_catalog = []
 
     # Объединение таблиц
-    merged_df = pd.merge(nomenklaturaold, photoadress, on='kod', how='left')
-    merged_df = pd.merge(merged_df, stockold, on='kod', how='left')
+    merged_df = pd.merge(nomenklaturaold, photoadress, on="kod", how="left")
+    merged_df = pd.merge(merged_df, stockold, on="kod", how="left")
 
     # Преобразование колонки stockold в float и заполнение NaN значений нулями
     merged_df["osnsklad"] = merged_df["osnsklad"].astype(float).fillna(0)
-    # merged_df.to_excel('merged.xlsx')
 
     # Фильтрация данных
     filtered_df = merged_df[
-        (merged_df['osnsklad'] > 0) & 
-        (merged_df['adress'].isna() | merged_df['adress'].str.strip().eq(''))
+        (merged_df["osnsklad"] > 0)
+        & (merged_df["adress"].isna() | merged_df["adress"].str.strip().eq(""))
     ]
 
     # Условия для фильтрации строк по 'naimenovanie'
@@ -132,25 +173,40 @@ def get_and_process_data():
         "FELIX",
         "Шина",
     ]
-
     exclude_contains = ["масло моторное", "масло трансмиссионное"]
 
     # Фильтрация строк
     filtered_df = filtered_df[
-        ~filtered_df['naimenovanie'].str.startswith(tuple(exclude_starts)) &
-        ~filtered_df['naimenovanie'].str.contains('|'.join(exclude_contains), case=False, na=False)
+        ~filtered_df["naimenovanie"].str.startswith(tuple(exclude_starts))
+        & ~filtered_df["naimenovanie"].str.contains(
+            "|".join(exclude_contains), case=False, na=False
+        )
     ]
 
+    # Оставляем только те строки, где kod есть в catalog, если catalog был загружен
+    if len(kod_catalog) > 0:
+        filtered_df = filtered_df[filtered_df["kod"].isin(kod_catalog)]
+
     # Выбор необходимых колонок, включая osnsklad
-    result_df = filtered_df[['kod', 'artikul', 'proizvoditel', 'naimenovanie', 'edizm', 'stellazh', 'osnsklad']]
+    result_df = filtered_df[
+        [
+            "kod",
+            "artikul",
+            "proizvoditel",
+            "naimenovanie",
+            "edizm",
+            "stellazh",
+            "osnsklad",
+        ]
+    ]
 
     # Сортировка по колонке 'naimenovanie'
-    result_df = result_df.sort_values(by='naimenovanie')
+    result_df = result_df.sort_values(by="naimenovanie")
 
     # Попытка сохранить файл в первую директорию
-    output_dir1 = r'D:\NAS\заказы\Евгений'
-    output_dir2 = r'\\26.218.196.12\заказы\Евгений'
-    file_name = 'ПозициибезФотоСайт.xlsx'
+    output_dir1 = r"D:\NAS\заказы\Евгений"
+    output_dir2 = r"\\26.218.196.12\заказы\Евгений"
+    file_name = "ПозициибезФотоСайт.xlsx"
     output_path = os.path.join(output_dir1, file_name)
 
     try:
@@ -165,6 +221,7 @@ def get_and_process_data():
             print("Обе директории недоступны. Файл не сохранен.")
     except Exception as e:
         print(f"Произошла ошибка при сохранении файла: {e}")
+
 
 # Основной исполняемый код
 if __name__ == "__main__":
