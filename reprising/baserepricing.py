820383973f32519718c965c206ebd6241ee4a0d8
Evgeni Stroikin <evgenistroikin@gmail.com>
2025-03-10 09:14:59 +0700

    Базовую наценку изменил до 1.6

--- reprising/baserepricing.py
+++ reprising/baserepricing.py
@@ -26,11 +26,11 @@ filtered_df, priceendmonth, salespivot, stockendmonth, suppliespivot, postupleni
 
 print("Пересчитываем цены для прайсов от поставщиков")
 # Пересчитываем цены для прайсов от поставщиков
-filtered = not_api_calculate_new_prices(filtered_df, salespivot, base_percent=1.55, reduced_base_percent=1.40)
+filtered = not_api_calculate_new_prices(filtered_df, salespivot, base_percent=1.6, reduced_base_percent=1.40)
 print("Базовая наценка от api")
 # filtered.to_csv("filtered_not_api_df.csv")
 # Базовая наценка от api
-df = calculate_new_prices_for_api(filtered, salespivot, suppliespivot, base_percent=1.55, reduced_base_percent=1.40)
+df = calculate_new_prices_for_api(filtered, salespivot, suppliespivot, base_percent=1.6, reduced_base_percent=1.40)
 # df.to_csv("filtered_df_api.csv")
 
 
